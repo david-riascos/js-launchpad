22d4aa79e5eed0b7215a1d6e7063ccc737d40b49
Emer Riascos <emer.riascos@pacific-hs.com>
2026-04-15 17:58:46 -0500

    Objects en Js

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,7 @@
+/*
+  Comentarios de multiples líneas
+  En JavaScript existen dos formas de escribir comentarios, los de una sola línea y los de múltiples líneas
+ */
+
+ // Comentario de una sola línea
+
---
+++ 01-tipos/06-objetos.js
@@ -0,0 +1,48 @@
+/*
+
+Son referencias en memoria, y se pueden representarse con llaves {}. 
+Pueden contener cualquier tipo de dato, incluso otros objetos. Se pueden 
+crear con la sintaxis literal o con el constructor Object().
+
+Los objetos pueden tener propiedades (datos) y métodos (funciones). Se accede 
+a las propiedades y métodos con la notación de punto o corchetes. Se pueden 
+modificar, agregar o eliminar propiedades y métodos en tiempo de ejecución.
+
+todos datos o información que conviene agrupar, organizar o representar como 
+un conjunto de características o comportamientos relacionados, se puede representar 
+como un objeto en JavaScript. 
+
+Esto incluye cosas como personas, animales, vehículos, 
+productos, eventos, etc. 
+
+Los objetos son una parte fundamental de la programación 
+orientada a objetos y permiten modelar el mundo real de manera más intuitiva y estructurada.
+
+
+*/
+
+// Ejemplo de un objeto literal
+
+const persona = {
+    nombre: "Juan",
+    edad: 30,
+    esEstudiante: true,
+    saludar: function() {
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+};
+
+console.log(persona.nombre); // Juan
+console.log(persona["edad"]); // 30
+persona.saludar(); // Hola, mi nombre es Juan y tengo 30 años.
+
+// Ejemplo de un objeto creado con el constructor Object()
+
+const coche = new Object();
+coche.marca = "Toyota";
+coche.modelo = "Corolla";
+coche.año = 2020;
+console.log(coche.marca); // Toyota
+console.log(coche["modelo"]); // Corolla
+console.log(coche["año"]); // 2020
+
